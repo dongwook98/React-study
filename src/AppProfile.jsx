8cad34e686d3b5f50369fbeed67c154c2a096e99
dongwook98 <ehddnr8563@gmail.com>
2023-03-09 23:15:17 +0900

    리액트에서 event 처리

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -3,8 +3,13 @@ import Avatar from './components/Avatar';
 import Profile from './components/Profile';
 
 function AppProfile() {
+  const handleClick = (event) => {
+    console.log(event);
+    alert('버튼이 클릭됫다');
+  };
   return (
     <>
+      <button onClick={handleClick}>버튼</button>
       <Profile
         image='https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80'
         name='James Kim'
@@ -24,7 +29,11 @@ function AppProfile() {
         isNew={true}
         isBest={true}
       />
-      <Avatar image='https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80' />
+      <Avatar
+        image='https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80'
+        isNew={true}
+        isBest={true}
+      />
     </>
   );
 }
